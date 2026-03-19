6be1b7ccd3d485d9597f76845afae001adde11bb
Rishikesh <DarnuleRishikesh@gmail.com>
2026-03-19 10:12:32 +0530

    learning socket.io

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const http = require('http')
+const app = express();
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('User Connected')
+    socket.broadcast.emit("connected")
+    socket.on('disconnect', () => socket.broadcast.emit('Disconnected'))
+})
+
+server.listen(PORT, () => {
+    console.log("Listening at ",PORT)
+})
